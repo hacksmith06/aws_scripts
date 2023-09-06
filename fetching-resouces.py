69d97752599705b99c7b74cf6df52ee1ea0acde9
hacksmith06 <singharpit0610@gmail.com>
2023-09-06 12:36:04 +0530

    checks if tag is not present then only it applies it

--- fetching-resouces.py
+++ fetching-resouces.py
@@ -30,13 +30,22 @@ tags = [
 # Initialize a tqdm progress bar with the total number of resources
 progress_bar = tqdm(total=len(resources['ResourceTagMappingList']), desc='Tagging Resources')
 
+
+# Helper function to check if a resource already has the specified tags
+def has_tags(resource_tags, tags_to_check):
+    return all(tag in resource_tags for tag in tags_to_check)
+
+
 # Iterate through the resources and tag them
 for resource in resources['ResourceTagMappingList']:
     resource_arn = resource['ResourceARN']
 
     try:
-        # Attempt to tag the resource with the three specified tags
-        client.tag_resources(ResourceARNList=[resource_arn], Tags=tags)
+        # Check if the resource already has the specified tags
+        existing_tags = client.get_resource_tags(ResourceARN=resource_arn)
+        if not has_tags(existing_tags['Tags'], tags):
+            # Tags are not present, so apply them
+            client.tag_resources(ResourceARNList=[resource_arn], Tags=tags)
     except Exception as e:
         # Handle the exception (e.g., permission issue)
         print(f"Error tagging resource {resource_arn}: {str(e)}")
